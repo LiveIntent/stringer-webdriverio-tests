e33c81b5730165baaf4eae86b209361d8f297db9
Vinod M <vinodmuralidharan@yahoo.com>
2022-05-09 09:13:52 +0200

    eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,9 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended"
-    ],
+      ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
